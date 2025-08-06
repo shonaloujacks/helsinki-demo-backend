d6d244d148255fd9ffd9dbe8da80fa06eea70748
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-08-06 16:36:41 +0100

    .env file added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
 const Note = require("./models/note");
+const app = express();
 
 let notes = [
   {
@@ -90,7 +91,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
